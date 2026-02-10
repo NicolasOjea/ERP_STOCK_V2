06a46de30f9269b41688de14ddc9ccb6fb330fe5
Nicolas Oscar Ojea Stefani <69313792+NicolasOjea@users.noreply.github.com>
2026-02-10 11:57:52 -0300

    drg

--- frontend/pos-ui/dev-dist/sw.js
+++ frontend/pos-ui/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.k496r5v7jd8"
+    "revision": "0.boci9v3mq28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
