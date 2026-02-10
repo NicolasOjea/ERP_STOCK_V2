2a88be32cd47ae8f3db48726da525d2c5382d0aa
Nicolas Oscar Ojea Stefani <69313792+NicolasOjea@users.noreply.github.com>
2026-02-10 11:39:50 -0300

    dockerfile

--- frontend/pos-ui/dev-dist/sw.js
+++ frontend/pos-ui/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.91p8u7divjg"
+    "revision": "0.k496r5v7jd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
