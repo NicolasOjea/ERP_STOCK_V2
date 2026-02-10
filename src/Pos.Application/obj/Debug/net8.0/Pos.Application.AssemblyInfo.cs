fceb2891cd0e2f76126a83630964bb680dd9b32f
Nicolas Oscar Ojea Stefani <69313792+NicolasOjea@users.noreply.github.com>
2026-02-10 18:26:57 -0300

    dba

--- src/Pos.Application/obj/Debug/net8.0/Pos.Application.AssemblyInfo.cs
+++ src/Pos.Application/obj/Debug/net8.0/Pos.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pos.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e2c680f40faa869a216977afb432508d07afc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba4767f2ae0b1d888b8fcf0013e359453dd1ac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pos.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pos.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
